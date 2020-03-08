a8332b785254e27e5fc69911a282d77426d73f87
Ikramuddin21 <shaonakhand2122@gmail.com>
2020-03-09 02:52:20 +0600

    update

---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if(result > 9){
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+    
+
+
+}
+const output = sum(4, 6);
+
+// console.log(bonus);
+// console.log(output);
